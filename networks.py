51f65c4c96b328208b829fa2b31ffd107d53c67b
Vladyslav Hladchenko <hladcvla@fel.cvut.cz>
2022-04-21 12:05:09 +0200

    add Net2

--- networks.py
+++ networks.py
@@ -39,3 +39,55 @@ class Net(nn.Module):
         x = self.fc(x)
         x = F.log_softmax(x, dim=1)
         return x
+
+
+class Net2(nn.Module):
+    def __init__(self):
+        super(Net2, self).__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(0.25)
+        )
+
+        self.features = nn.Sequential(
+            self.conv1,
+            self.conv2,
+            self.conv3,
+            self.conv4
+        )
+
+        linear_in_size = get_linear_in_size(28, self.features)
+
+        self.fc = nn.Sequential(
+            nn.Linear(linear_in_size, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        x = F.log_softmax(x, dim=1)
+        return x
